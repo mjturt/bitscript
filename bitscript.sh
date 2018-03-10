36019e4d8117c650df9fac0d1a3ffde3c2dc6686
sigmap <soclio.zecter@gmail.com>
2018-03-10 03:28:45 -0600

    Update bitscript.sh
    5 minutes is 300 seconds

--- bitscript.sh
+++ bitscript.sh
@@ -3,5 +3,5 @@
 while [ true ]
 do
     echo "$(date +%d.%m.%Y-%H:%M) $(curl -s https://www.bitstamp.net/api/ticker/ | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'last'"]';)" >> bit.txt
-    sleep 360
+    sleep 300
 done
